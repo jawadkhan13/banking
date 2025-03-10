61e7ad641758af3cf8568b724c2f00c5cd1ad1d8
Jawad Khane <jawadkhan.cse@gmail.com>
2025-03-10 15:12:31 +0500

    File & Folder structure

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  return (
+    <main className="flex h-screen w-full font-inter">
+        {children} 
+    </main>
+  );
+}
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  return (
+    <main className="flex h-screen w-full font-inter">
+      SIDEBAR
+        {children}
+    
+    </main>
+  );
+}
