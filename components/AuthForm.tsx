b453ff46e431353bcab35c990d854aa9d5788550
Jawad Khane <jawadkhan.cse@gmail.com>
2025-03-15 00:40:19 +0500

    Implement PlaidLink and Dwolla

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -23,6 +23,7 @@ import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { signIn, signUp } from '@/lib/actions/user.actions';
+import PlaidLink from './PlaidLink';
 
 
 const AuthForm = ({type}: {type: string}) => {
@@ -45,8 +46,22 @@ const AuthForm = ({type}: {type: string}) => {
     setIsLoading(true)
     try {
       // Sign up with Appwrite & create plaid token
-      if (type === 'sign-up') {
-        const newUser = await signUp(data)
+
+      if(type === 'sign-up') {
+        const userData = {
+          firstName: data.firstName!,
+          lastName: data.lastName!,
+          address1: data.address1!,
+          city: data.city!,
+          state: data.state!,
+          postalCode: data.postalCode!,
+          dateOfBirth: data.dateOfBirth!,
+          ssn: data.ssn!,
+          email: data.email,
+          password: data.password
+        }
+
+        const newUser = await signUp(userData);
 
         setUser(newUser)
       }
@@ -96,7 +111,7 @@ const AuthForm = ({type}: {type: string}) => {
       </header>
       {user ? (
         <div className="flex flex-col gap-4">
-          {/* <PlaidLink user={user} variant="primary" /> */}
+          <PlaidLink user={user} variant="primary" />
         </div>
       ): (
         <>
@@ -150,7 +165,7 @@ const AuthForm = ({type}: {type: string}) => {
             </Link>
           </footer>
         </>
-      )}
+       )} 
     </section>
   );
 }
